41121c32af337bbcda8f8d8358066b236e670282
Mahendhar <7093741662mahiv@gmail.com>
2024-03-01 15:24:26 +0530

    ca_8

--- src/pages/FormsPage.js
+++ src/pages/FormsPage.js
@@ -9,7 +9,7 @@ function FormsPage() {
   return (
     <div>
       <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Forms</h1>
-      <CTA />
+   
       <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Elements</h2>
 
       <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 microInteractionsContainer">
@@ -130,6 +130,21 @@ function FormsPage() {
             </div>
           </div>
         </Label>
+
+        <Label className="inputWithIcon">
+          <span>Icon left</span>
+          {/* <!-- focus-within sets the color for the icon when input is focused --> */}
+          <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+            <input
+              className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+              placeholder="Jane Doe"
+            />
+            <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
+              <MailIcon className="w-5 h-5" aria-hidden="true" />
+            </div>
+          </div>
+        </Label>
+  
       </div>
 
       {/* <!-- Inputs with buttons --> */}
