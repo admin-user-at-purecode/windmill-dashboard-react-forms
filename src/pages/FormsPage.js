1a7d6af166923a338b9aa2e203e7c1953e462352
Mahendhar <7093741662mahiv@gmail.com>
2024-03-01 18:32:47 +0530

    la_22

--- src/pages/FormsPage.js
+++ src/pages/FormsPage.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React from "react";
 
-import { Input, HelperText, Label, Select, Textarea } from '@windmill/react-ui'
+import { Input, HelperText, Label, Select, Textarea } from "@windmill/react-ui";
 
-import { MailIcon } from '../assets/icons'
-import CTA from '../components/CTA'
+import { MailIcon } from "../assets/icons";
+import CTA from "../components/CTA";
 
 function FormsPage() {
   return (
     <div>
-      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Forms</h1>
-      <CTA />
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Elements</h2>
+      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">
+        Forms
+      </h1>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Validation
+      </h2>
+
+      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 validationForm">
+        <Label className="input">
+          <span>Invalid input</span>
+          <Input className="mt-1" valid={false} placeholder="Jane Doe" />
+          <HelperText valid={false}>Your password is too short.</HelperText>
+        </Label>
+
+        <Label className="mt-4 input">
+          <span>Valid input</span>
+          <Input className="mt-1" valid={true} placeholder="Jane Doe" />
+          <HelperText valid={true}>Your password is strong.</HelperText>
+        </Label>
+
+        <Label className="mt-4 input">
+          <span>Helper text</span>
+          <Input className="mt-1" placeholder="Jane Doe" />
+          <HelperText>
+            Your password must be at least 6 characters long.
+          </HelperText>
+        </Label>
+      </div>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Elements
+      </h2>
 
       <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 microInteractionsContainer">
         <Label className="nameInput">
@@ -37,7 +67,12 @@ function FormsPage() {
               <span className="ml-2">Business</span>
             </Label>
             <Label disabled className="ml-6" radio>
-              <Input disabled type="radio" value="disabled" name="accountType" />
+              <Input
+                disabled
+                type="radio"
+                value="disabled"
+                name="accountType"
+              />
               <span className="ml-2">Disabled</span>
             </Label>
           </div>
@@ -66,7 +101,11 @@ function FormsPage() {
 
         <Label className="mt-4 textarea">
           <span>Message</span>
-          <Textarea className="mt-1" rows="3" placeholder="Enter some long form content." />
+          <Textarea
+            className="mt-1"
+            rows="3"
+            placeholder="Enter some long form content."
+          />
         </Label>
 
         <Label className="mt-6 checkbox" check>
@@ -77,30 +116,41 @@ function FormsPage() {
         </Label>
       </div>
 
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Validation</h2>
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Buttons
+      </h2>
 
-      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 validationForm">
-        <Label className="input">
-          <span>Invalid input</span>
-          <Input className="mt-1" valid={false} placeholder="Jane Doe" />
-          <HelperText valid={false}>Your password is too short.</HelperText>
+      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithButtonContainer">
+        <Label className="inputWithButton">
+          <span>Button left</span>
+          <div className="relative">
+            <input
+              className="block w-full pl-20 mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+              placeholder="Jane Doe"
+            />
+            <button className="absolute inset-y-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-l-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
+              Click
+            </button>
+          </div>
         </Label>
 
-        <Label className="mt-4 input">
-          <span>Valid input</span>
-          <Input className="mt-1" valid={true} placeholder="Jane Doe" />
-          <HelperText valid={true}>Your password is strong.</HelperText>
-        </Label>
-
-        <Label className="mt-4 input">
-          <span>Helper text</span>
-          <Input className="mt-1" placeholder="Jane Doe" />
-          <HelperText>Your password must be at least 6 characters long.</HelperText>
+        <Label className="mt-4 inputWithButton">
+          <span>Button right</span>
+          <div className="relative text-gray-500 focus-within:text-purple-600">
+            <input
+              className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+              placeholder="Jane Doe"
+            />
+            <button className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+              Click
+            </button>
+          </div>
         </Label>
       </div>
 
-      {/* <!-- Inputs with icons --> */}
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Icons</h2>
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Icons
+      </h2>
 
       <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithIconContainer">
         <Label className="inputWithIcon">
@@ -131,39 +181,8 @@ function FormsPage() {
           </div>
         </Label>
       </div>
-
-      {/* <!-- Inputs with buttons --> */}
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Buttons</h2>
-
-      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithButtonContainer">
-        <Label className="inputWithButton">
-          <span>Button left</span>
-          <div className="relative">
-            <input
-              className="block w-full pl-20 mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-              placeholder="Jane Doe"
-            />
-            <button className="absolute inset-y-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-l-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
-              Click
-            </button>
-          </div>
-        </Label>
-
-        <Label className="mt-4 inputWithButton">
-          <span>Button right</span>
-          <div className="relative text-gray-500 focus-within:text-purple-600">
-            <input
-              className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-              placeholder="Jane Doe"
-            />
-            <button className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-              Click
-            </button>
-          </div>
-        </Label>
-      </div>
     </div>
-  )
+  );
 }
 
-export default FormsPage
+export default FormsPage;
