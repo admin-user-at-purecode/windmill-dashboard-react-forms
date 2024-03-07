a0789a709f8620cf0a5bdfec65edba7f2ec51377
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-07 13:01:45 +0530

    ca_11

---
+++ src/components/cards/InfoCard.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Card, CardBody } from '@windmill/react-ui'
+import classNames from 'classnames'
+
+function InfoCard({ title, value, children: icon }) {
+  return (
+    <Card>
+      <CardBody className="flex items-center">
+        {icon}
+        <div>
+          <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">{title}</p>
+          <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">{value}</p>
+        </div>
+      </CardBody>
+    </Card>
+  )
+}
+
+export function CTA() {
+  return (
+    <a
+      className="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple"
+    >
+      <div className="flex items-center">
+        <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+        <span>Star this project on GitHub</span>
+      </div>
+      <span>
+        View more <span dangerouslySetInnerHTML={{ __html: '&RightArrow;' }}></span>
+      </span>
+    </a>
+  )
+}
+
+export function RoundIcon({
+  icon: Icon,
+  iconColorClass = 'text-purple-600 dark:text-purple-100',
+  bgColorClass = 'bg-purple-100 dark:bg-purple-600',
+  className,
+}) {
+  const baseStyle = 'p-3 rounded-full'
+
+  const cls = classNames(baseStyle, iconColorClass, bgColorClass, className)
+  return (
+    <div className={cls}>
+      <Icon className="w-5 h-5" />
+    </div>
+  )
+}
+
+export default InfoCard
--- src/pages/FormsPage.js
+++ src/pages/FormsPage.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 import { Input, HelperText, Label, Select, Textarea } from '@windmill/react-ui'
 
-import { MailIcon } from '../assets/icons'
+import { MailIcon, CartIcon, ChatIcon, MoneyIcon, PeopleIcon  } from '../assets/icons'
 import CTA from '../components/CTA'
+import InfoCard, {  RoundIcon } from '../components/cards/InfoCard'
 
 function FormsPage() {
   return (
@@ -98,6 +99,47 @@ function FormsPage() {
           <HelperText>Your password must be at least 6 characters long.</HelperText>
         </Label>
       </div>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Responsive cards</h2>
+   
+
+   <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+     <InfoCard title="Total clients" value="6389">
+       <RoundIcon
+         icon={PeopleIcon}
+         iconColorClass="text-orange-500 dark:text-orange-100"
+         bgColorClass="bg-orange-100 dark:bg-orange-500"
+         className="mr-4"
+       />
+     </InfoCard>
+
+     <InfoCard title="Account balance" value="$ 46,760.89">
+       <RoundIcon
+         icon={MoneyIcon}
+         iconColorClass="text-green-500 dark:text-green-100"
+         bgColorClass="bg-green-100 dark:bg-green-500"
+         className="mr-4"
+       />
+     </InfoCard>
+
+     <InfoCard title="New sales" value="376">
+       <RoundIcon
+         icon={CartIcon}
+         iconColorClass="text-blue-500 dark:text-blue-100"
+         bgColorClass="bg-blue-100 dark:bg-blue-500"
+         className="mr-4"
+       />
+     </InfoCard>
+
+     <InfoCard title="Pending contacts" value="35">
+       <RoundIcon
+         icon={ChatIcon}
+         iconColorClass="text-teal-500 dark:text-teal-100"
+         bgColorClass="bg-teal-100 dark:bg-teal-500"
+         className="mr-4"
+       />
+     </InfoCard>
+   </div>
 
       {/* <!-- Inputs with icons --> */}
       <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Icons</h2>
