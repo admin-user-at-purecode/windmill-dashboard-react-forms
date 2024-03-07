e5b9a8bbe4585252d77944fb61bc1abd110a9529
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-07 16:47:36 +0530

    cr_6

--- src/pages/FormsPage.js
+++ src/pages/FormsPage.js
@@ -77,91 +77,103 @@ function FormsPage() {
         </Label>
       </div>
 
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Validation</h2>
+      <div className='grid md:grid-cols-2 gap-4'>
+        <div>
+        <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Validation</h2>
 
-      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 validationForm">
-        <Label className="input">
-          <span>Invalid input</span>
-          <Input className="mt-1" valid={false} placeholder="Jane Doe" />
-          <HelperText valid={false}>Your password is too short.</HelperText>
-        </Label>
+<div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 validationForm">
+  <Label className="input">
+    <span>Invalid input</span>
+    <Input className="mt-1" valid={false} placeholder="Jane Doe" />
+    <HelperText valid={false}>Your password is too short.</HelperText>
+  </Label>
 
-        <Label className="mt-4 input">
-          <span>Valid input</span>
-          <Input className="mt-1" valid={true} placeholder="Jane Doe" />
-          <HelperText valid={true}>Your password is strong.</HelperText>
-        </Label>
+  <Label className="mt-4 input">
+    <span>Valid input</span>
+    <Input className="mt-1" valid={true} placeholder="Jane Doe" />
+    <HelperText valid={true}>Your password is strong.</HelperText>
+  </Label>
 
-        <Label className="mt-4 input">
-          <span>Helper text</span>
-          <Input className="mt-1" placeholder="Jane Doe" />
-          <HelperText>Your password must be at least 6 characters long.</HelperText>
-        </Label>
+  <Label className="mt-4 input">
+    <span>Helper text</span>
+    <Input className="mt-1" placeholder="Jane Doe" />
+    <HelperText>Your password must be at least 6 characters long.</HelperText>
+  </Label>
+</div>
+        </div>
+        <div>
+        <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Icons</h2>
+
+<div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithIconContainer">
+  <Label className="inputWithIcon">
+    <span>Icon left</span>
+    {/* <!-- focus-within sets the color for the icon when input is focused --> */}
+    <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+      <input
+        className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+        placeholder="Jane Doe"
+      />
+      <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
+        <MailIcon className="w-5 h-5" aria-hidden="true" />
       </div>
+    </div>
+  </Label>
+
+  <Label className="mt-4 inputWithIcon">
+    <span className="text-gray-700 dark:text-gray-400">Icon right</span>
+    {/* <!-- focus-within sets the color for the icon when input is focused --> */}
+    <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+      <input
+        className="block w-full pr-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+        placeholder="Jane Doe"
+      />
+      <div className="absolute inset-y-0 right-0 flex items-center mr-3 pointer-events-none">
+        <MailIcon className="w-5 h-5" aria-hidden="true" />
+      </div>
+    </div>
+  </Label>
+</div>
+</div>
+        <div>
+        <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Buttons</h2>
+
+<div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithButtonContainer">
+  <Label className="inputWithButton">
+    <span>Button left</span>
+    <div className="relative">
+      <input
+        className="block w-full pl-20 mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+        placeholder="Jane Doe"
+      />
+      <button className="absolute inset-y-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-l-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
+        Click
+      </button>
+    </div>
+  </Label>
+
+  <Label className="mt-4 inputWithButton">
+    <span>Button right</span>
+    <div className="relative text-gray-500 focus-within:text-purple-600">
+      <input
+        className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+        placeholder="Jane Doe"
+      />
+      <button className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+        Click
+      </button>
+    </div>
+  </Label>
+</div>
+        </div>
+      </div>
+
+
 
       {/* <!-- Inputs with icons --> */}
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Icons</h2>
 
-      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithIconContainer">
-        <Label className="inputWithIcon">
-          <span>Icon left</span>
-          {/* <!-- focus-within sets the color for the icon when input is focused --> */}
-          <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
-            <input
-              className="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-              placeholder="Jane Doe"
-            />
-            <div className="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
-              <MailIcon className="w-5 h-5" aria-hidden="true" />
-            </div>
-          </div>
-        </Label>
-
-        <Label className="mt-4 inputWithIcon">
-          <span className="text-gray-700 dark:text-gray-400">Icon right</span>
-          {/* <!-- focus-within sets the color for the icon when input is focused --> */}
-          <div className="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
-            <input
-              className="block w-full pr-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-              placeholder="Jane Doe"
-            />
-            <div className="absolute inset-y-0 right-0 flex items-center mr-3 pointer-events-none">
-              <MailIcon className="w-5 h-5" aria-hidden="true" />
-            </div>
-          </div>
-        </Label>
-      </div>
 
       {/* <!-- Inputs with buttons --> */}
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Buttons</h2>
 
-      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithButtonContainer">
-        <Label className="inputWithButton">
-          <span>Button left</span>
-          <div className="relative">
-            <input
-              className="block w-full pl-20 mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-              placeholder="Jane Doe"
-            />
-            <button className="absolute inset-y-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-l-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
-              Click
-            </button>
-          </div>
-        </Label>
-
-        <Label className="mt-4 inputWithButton">
-          <span>Button right</span>
-          <div className="relative text-gray-500 focus-within:text-purple-600">
-            <input
-              className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-              placeholder="Jane Doe"
-            />
-            <button className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-              Click
-            </button>
-          </div>
-        </Label>
-      </div>
     </div>
   )
 }
